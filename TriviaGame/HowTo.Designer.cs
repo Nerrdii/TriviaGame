82247ea34b1b4c38f270e2c7bacce8cd19d03e11
Paul <pwiltz@gmail.com>
2018-11-10 07:13:06 -0600

    Added how to instructions

--- TriviaGame/HowTo.Designer.cs
+++ TriviaGame/HowTo.Designer.cs
@@ -29,61 +29,73 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.yesButton = new System.Windows.Forms.Button();
-            this.noButton = new System.Windows.Forms.Button();
+            this.fullButton = new System.Windows.Forms.Button();
             this.howTBackButton = new System.Windows.Forms.Button();
+            this.quickButton = new System.Windows.Forms.Button();
+            this.howtoLabeltext = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Bauhaus 93", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Location = new System.Drawing.Point(52, 8);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(326, 30);
+            this.label1.Size = new System.Drawing.Size(239, 30);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Do you need instructions?";
+            this.label1.Text = "Game instructions!";
             // 
-            // yesButton
+            // fullButton
             // 
-            this.yesButton.Font = new System.Drawing.Font("Bauhaus 93", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.yesButton.Location = new System.Drawing.Point(70, 77);
-            this.yesButton.Name = "yesButton";
-            this.yesButton.Size = new System.Drawing.Size(86, 36);
-            this.yesButton.TabIndex = 1;
-            this.yesButton.Text = "Yes";
-            this.yesButton.UseVisualStyleBackColor = true;
-            // 
-            // noButton
-            // 
-            this.noButton.Font = new System.Drawing.Font("Bauhaus 93", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.noButton.Location = new System.Drawing.Point(173, 77);
-            this.noButton.Name = "noButton";
-            this.noButton.Size = new System.Drawing.Size(86, 36);
-            this.noButton.TabIndex = 2;
-            this.noButton.Text = "No";
-            this.noButton.UseVisualStyleBackColor = true;
-            this.noButton.Click += new System.EventHandler(this.noButton_Click);
+            this.fullButton.Font = new System.Drawing.Font("Bauhaus 93", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fullButton.Location = new System.Drawing.Point(79, 43);
+            this.fullButton.Name = "fullButton";
+            this.fullButton.Size = new System.Drawing.Size(86, 36);
+            this.fullButton.TabIndex = 1;
+            this.fullButton.Text = "Full";
+            this.fullButton.UseVisualStyleBackColor = true;
+            this.fullButton.Click += new System.EventHandler(this.fullButton_Click);
             // 
             // howTBackButton
             // 
             this.howTBackButton.Font = new System.Drawing.Font("Bauhaus 93", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.howTBackButton.Image = global::TriviaGame.Properties.Resources.backB;
-            this.howTBackButton.Location = new System.Drawing.Point(123, 353);
+            this.howTBackButton.Location = new System.Drawing.Point(129, 368);
             this.howTBackButton.Name = "howTBackButton";
             this.howTBackButton.Size = new System.Drawing.Size(87, 44);
             this.howTBackButton.TabIndex = 3;
             this.howTBackButton.UseVisualStyleBackColor = true;
             this.howTBackButton.Click += new System.EventHandler(this.howTBackButton_Click);
             // 
+            // quickButton
+            // 
+            this.quickButton.Font = new System.Drawing.Font("Bauhaus 93", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quickButton.Location = new System.Drawing.Point(171, 43);
+            this.quickButton.Name = "quickButton";
+            this.quickButton.Size = new System.Drawing.Size(86, 36);
+            this.quickButton.TabIndex = 4;
+            this.quickButton.Text = "Quick";
+            this.quickButton.UseVisualStyleBackColor = true;
+            this.quickButton.Click += new System.EventHandler(this.quickButton_Click);
+            // 
+            // howtoLabeltext
+            // 
+            this.howtoLabeltext.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.howtoLabeltext.Font = new System.Drawing.Font("Berlin Sans FB Demi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.howtoLabeltext.Location = new System.Drawing.Point(13, 82);
+            this.howtoLabeltext.Name = "howtoLabeltext";
+            this.howtoLabeltext.Size = new System.Drawing.Size(313, 283);
+            this.howtoLabeltext.TabIndex = 5;
+            // 
             // HowTo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(338, 436);
+            this.Controls.Add(this.howtoLabeltext);
+            this.Controls.Add(this.quickButton);
             this.Controls.Add(this.howTBackButton);
-            this.Controls.Add(this.noButton);
-            this.Controls.Add(this.yesButton);
+            this.Controls.Add(this.fullButton);
             this.Controls.Add(this.label1);
             this.Name = "HowTo";
             this.Text = "How To ?";
@@ -95,8 +107,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button yesButton;
-        private System.Windows.Forms.Button noButton;
+        private System.Windows.Forms.Button fullButton;
         private System.Windows.Forms.Button howTBackButton;
+        private System.Windows.Forms.Button quickButton;
+        private System.Windows.Forms.Label howtoLabeltext;
     }
 }
--- TriviaGame/HowTo.cs
+++ TriviaGame/HowTo.cs
@@ -18,18 +18,22 @@ namespace TriviaGame
             this.StartPosition = FormStartPosition.Manual;
             this.Location = new Point(0, 0);
         }
-
-        private void noButton_Click(object sender, EventArgs e)
-        {
-            
-        }
-
         private void howTBackButton_Click(object sender, EventArgs e)
         {
             this.Hide();
             Main backMain = new Main();
             backMain.MdiParent = this.MdiParent;
             backMain.Show();            
+        }
+
+        private void fullButton_Click(object sender, EventArgs e)
+        {
+            howtoLabeltext.Text = "Just a place holder until real instructions are put in. So blah, blah blah and Maybe you're not sitting pretty with a Best Actor Oscar award, but you can win the Movie Buff Bragger title when you beat your friends and family at Movies Trivia game night!  Your family and friends will try to distract you with buttery popcorn, fizzy drinks and sugary concoctions, but you stay true to that movie brain of yours!  And when you need a short break, stand tall and tell em', I'LL BE BACK!.  That's right, they'll be quivering as you exit the room.";
+        }
+
+        private void quickButton_Click(object sender, EventArgs e)
+        {
+            howtoLabeltext.Text = "Man, stop being silly you know how to play a trivia game; Just Play!";
         }
     }
 }
